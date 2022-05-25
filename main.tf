c6aa1a5170623d9ade7888136834e0d1f8b49132
stpride <spride@gmail.com>
2022-05-25 10:13:20 -0500

    Initial files

---
+++ main.tf
@@ -0,0 +1,12 @@
+
+module "vpc-east-1" {
+  source      = "git@github.com:stpride/aws-module-vpc.git"
+  region      = "us-east-1"
+  environment = "dev"
+  name        = "VPC-Management-1"
+  cidr        = "10.0.0.0/16"
+  zones       = [ "us-east-1a" ]
+  private     = [ "10.0.48.0/20" ]
+  public      = [ "10.0.0.0/20" ]
+}
+
